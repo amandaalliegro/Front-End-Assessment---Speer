b5c7da921df59f22d66b3e2f16354b461ff55f45
amandaalliegro <amanda.m.arnaut@gmail.com>
2021-02-04 12:36:30 +0000

    starting home page

--- express_server.js
+++ express_server.js
@@ -5,6 +5,9 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use("/static", express.static("public"));
 
+app.get("/tryit", (req, res) => {
+  res.render("tryIt");
+});
 app.get("/", (req, res) => {
   res.render("index");
 });

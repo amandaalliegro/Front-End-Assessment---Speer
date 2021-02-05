c07895f532cd243b1985dd36b7a23e08885bb2c5
amandaalliegro <amanda.m.arnaut@gmail.com>
2021-02-05 03:18:00 +0000

    buttons change the background color when hovered

--- public/css/helper.js
+++ public/css/helper.js
@@ -1,5 +1,12 @@
 $(function () {
+  document.querySelector('.select-btn-white').onmousemove = function (e) {
+
+    var x = e.pageX - e.target.offsetLeft;
+    var y = e.pageY - e.target.offsetTop;
   
+    e.target.style.setProperty('--x', x + 'px');
+    e.target.style.setProperty('--y', y + 'px');
+  }
   
   let click = 0;
 
